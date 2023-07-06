11f3062d24c819d128460775169dfac1507668c7
Epsilon8854 <epsilon8854@unist.ac.kr>
2023-07-06 18:20:16 +0900

    change name

--- scripts/g2t.py
+++ scripts/g2t.py
@@ -7,16 +7,15 @@ class Listener:
     def __init__(self):
         rospy.init_node('g2t', anonymous=True)
         rospy.Subscriber("carter_msg_goal", PoseStamped, self.callback)
-        self.file = open("output.txt", "w")
+        self.file = open("goals.txt", "w")
 
     def callback(self, data):
-        
         message_str = "{}, {} ,{}, {}, {}, {}\n".format(
             data.pose.position.x, data.pose.position.y,
             data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w)
         rospy.loginfo(rospy.get_caller_id() + "I heard: \n" + message_str)
         self.file.write(message_str)
-
+        
     def listener_spin(self):
         rospy.spin()
         self.file.close()
